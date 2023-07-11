c1dc65e59b04d1cd0a78a92b4126853aa22873fc
deeprahman <dp.rahman@gmail.com>
2023-07-11 14:16:46 +0000

    build directory removed

--- build/utils/debug/deps/utils.d
+++
@@ -1,5 +0,0 @@
-/home/deep/Workspace/rust-mod/build/utils/debug/deps/libutils.so: src/includes/utils.rs
-
-/home/deep/Workspace/rust-mod/build/utils/debug/deps/utils.d: src/includes/utils.rs
-
-src/includes/utils.rs:
--- build/utils/debug/libutils.d
+++
@@ -1,1 +0,0 @@
-/home/deep/Workspace/rust-mod/build/utils/debug/libutils.so: /home/deep/Workspace/rust-mod/src/includes/utils.rs
